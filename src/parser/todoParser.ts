31a585ab631856b8d0bf43c7a4b24ab9c3658beb
manamasu <97730788+manamasu@users.noreply.github.com>
2026-06-03 23:06:17 +0200

    fix: support Python style # comments in TODO parser

--- src/parser/todoParser.ts
+++ src/parser/todoParser.ts
@@ -6,15 +6,16 @@ export function parseTodos(
   syncedKeys: string[],
 ): TodoItem[] {
   let lineCount = document.lineCount;
-  const regex = /\s*\/\/\s*(TODO|FIXME|HACK)(\[(high|medium|low)\])?:\s*(.+)/;
+  const regex =
+    /\s*(\/\/|\#)\s*(TODO|FIXME|HACK)(\[(high|medium|low)\])?:\s*(.+)/;
   const todoItems: TodoItem[] = [];
   for (let i = 0; i < lineCount; i++) {
     const match = regex.exec(document.lineAt(i).text);
     if (!match) continue; //no TODO, FIXME or HACK Found.
 
-    let type = match[1] as Type;
-    let priority = match[3] as Priority | undefined;
-    let message = match[4];
+    let type = match[2] as Type;
+    let priority = match[4] as Priority | undefined;
+    let message = match[5];
     const key = `${document.fileName}:${message}`;
 
     todoItems.push({
